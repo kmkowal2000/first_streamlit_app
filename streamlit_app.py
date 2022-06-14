10ffedbd77f387bc0ffb5e9b3b9f6420877f96f7
kmkowal2000 <107416176+kmkowal2000@users.noreply.github.com>
2022-06-14 09:35:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner');
 streamlit.header('Breakfast menu');
-streamlit.text('position 1);
-streamlit.text('position 2);
-streamlit.text('position 3);
+streamlit.text('position 1');
+streamlit.text('position 2');
+streamlit.text('position 3');
 
 
